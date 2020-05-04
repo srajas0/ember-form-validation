b1e882576780403529a810d66e8b17b10b7c33cd
Raja <srajas02@gmail.com>
2020-05-04 21:43:38 +0530

    remove unused variable

--- app/models/user.js
+++ app/models/user.js
@@ -52,60 +52,6 @@ models.forEach(model => {
 
 const Validations = buildValidations(assign({}, buildValidation));
 
-const Validationss = buildValidations(
-  {
-    name: {
-      validators: [
-        validator('presence', {
-          presence: true,
-          message: `Name is required`
-        }),
-        validator('format', {
-          regex: /^[a-zA-Z]+$/,
-          message: 'The name field should have alphabets only'
-        }),
-        validator('length', {
-          isWarning: true,
-          min: 2,
-          message: 'Name should have atleast two characters'
-        })
-      ]
-    },
-    email: {
-      validators: [
-        validator('presence', {
-          presence: true,
-          message: `Email is required`
-        }),
-        validator('format', {
-          // type: 'email',
-          regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          message: 'The email is not valid format'
-        })
-      ]
-    },
-    mobileNumber: {
-      validators: [
-        validator('presence', {
-          presence: true,
-          message: `Mobile Number is required`
-        }),
-        validator('format', {
-          regex: /^[0-9]+$/,
-          message: 'The mobileNumber field should have alphabets only'
-        }),
-        validator('length', {
-          isWarning: true,
-          min: 2,
-          max: 10,
-          message: 'Mobile Number should be 2 - 10 numbers only'
-        })
-      ]
-    }
-  }
-);
-
-
 export default DS.Model.extend(Validations, {
   name: DS.attr('string'),
   email: DS.attr('string'),
